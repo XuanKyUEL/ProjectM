fc836240673dd2e0bf054c01c045974a07b2b5b1
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-15 15:37:00 +0700

    initial commit

---
+++ app/src/main/java/mnxk/kotlintex/projectm/SplashActivity.kt
@@ -0,0 +1,11 @@
+package mnxk.kotlintex.projectm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
