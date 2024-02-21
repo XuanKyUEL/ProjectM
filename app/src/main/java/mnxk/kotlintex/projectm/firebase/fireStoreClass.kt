21ff2d10129e0e95f61612d34beef51c46e3c73e
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-21 11:21:55 +0700

    Fea: Auto login

--- app/src/main/java/mnxk/kotlintex/projectm/firebase/fireStoreClass.kt
+++ app/src/main/java/mnxk/kotlintex/projectm/firebase/fireStoreClass.kt
@@ -28,7 +28,12 @@ class fireStoreClass {
     }
 
     fun getCurrentUserId(): String {
-        return FirebaseAuth.getInstance().currentUser!!.uid
+        var currentUser = FirebaseAuth.getInstance().currentUser
+        var currentUserID = ""
+        if (currentUser != null) {
+            currentUserID = currentUser.uid
+        }
+        return currentUserID
     }
 
     fun sigInUser(
