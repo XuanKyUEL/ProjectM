63144d9dedb1b5cd269b6d06e614f5578f9eb94e
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-02-21 10:39:08 +0700

    Fea: Create User collection in FirestoreCloud

---
+++ app/src/main/java/mnxk/kotlintex/projectm/utils/Constants.kt
@@ -0,0 +1,10 @@
+package mnxk.kotlintex.projectm.utils
+
+object Constants {
+    const val USERS: String = "users"
+    const val PROJECTM_PREFERENCES: String = "ProjectMPrefs"
+    const val LOGGED_IN_USERNAME: String = "logged_in_username"
+    const val EXTRA_USER_DETAILS: String = "extra_user_details"
+    const val READ_STORAGE_PERMISSION_CODE = 2
+    const val PICK_IMAGE_REQUEST_CODE = 1
+}
