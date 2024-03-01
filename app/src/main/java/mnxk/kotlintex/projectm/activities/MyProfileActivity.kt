54a5c6a60303f2c8b4cc13b25bd4ba9f73e71b71
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-03-01 19:19:39 +0700

    Fea: Imagechooser

--- app/src/main/java/mnxk/kotlintex/projectm/activities/MyProfileActivity.kt
+++ app/src/main/java/mnxk/kotlintex/projectm/activities/MyProfileActivity.kt
@@ -1,6 +1,14 @@
 package mnxk.kotlintex.projectm.activities
 
+import android.Manifest.*
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import mnxk.kotlintex.projectm.R
 import mnxk.kotlintex.projectm.databinding.ActivityMyProfileBinding
@@ -16,6 +24,76 @@ class MyProfileActivity : BaseActivity() {
         setContentView(binding.root)
         setupActionBar()
         fireStoreClass().loadUserData(this)
+        binding.civProfileImage.setOnClickListener {
+            openImageChooser()
+        }
+    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == PICK_IMAGE_REQUEST_CODE) {
+//            if (resultCode == RESULT_OK) {
+//                if (data != null) {
+//                    try {
+//                        val selectedImageUri = data.data!!
+//                        Glide
+//                            .with(this)
+//                            .load(selectedImageUri)
+//                            .centerCrop()
+//                            .placeholder(R.drawable.ic_user_place_holder)
+//                            .into(binding.civProfileImage)
+//                    } catch (e: Exception) {
+//                        e.printStackTrace()
+//                    }
+//                }
+//            }
+//        }
+//    }
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                openImageChooser()
+            } else {
+                Toast.makeText(this, "Permission required to access images.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private val imagePickerActivityResult: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                if (result.data != null) {
+                    try {
+                        val selectedImageUri = result.data!!.data!!
+                        Glide
+                            .with(this)
+                            .load(selectedImageUri)
+                            .centerCrop()
+                            .placeholder(R.drawable.ic_user_place_holder)
+                            .into(binding.civProfileImage)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+
+    private fun openImageChooser() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                permission.READ_MEDIA_IMAGES,
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                val intent =
+                    Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                        addCategory(Intent.CATEGORY_OPENABLE)
+                        type = "image/*"
+                    }
+                imagePickerActivityResult.launch(intent)
+            }
+            else -> {
+                requestPermissionLauncher.launch(permission.READ_MEDIA_IMAGES)
+            }
+        }
     }
 
     private fun setupActionBar() {
