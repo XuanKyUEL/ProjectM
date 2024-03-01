778860efae9ffee9bd2686f0f7d802b01f19f513
XuanKyUEL <kymnx21411@st.uel.edu.vn>
2024-03-01 17:58:07 +0700

    Fea: show user data on profilescreen

--- app/src/main/java/mnxk/kotlintex/projectm/activities/MyProfileActivity.kt
+++ app/src/main/java/mnxk/kotlintex/projectm/activities/MyProfileActivity.kt
@@ -1,7 +1,11 @@
 package mnxk.kotlintex.projectm.activities
 
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import mnxk.kotlintex.projectm.R
 import mnxk.kotlintex.projectm.databinding.ActivityMyProfileBinding
+import mnxk.kotlintex.projectm.firebase.fireStoreClass
+import mnxk.kotlintex.projectm.models.User
 
 class MyProfileActivity : BaseActivity() {
     private lateinit var binding: ActivityMyProfileBinding
@@ -10,5 +14,38 @@ class MyProfileActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMyProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupActionBar()
+        fireStoreClass().loadUserData(this)
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbar)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = resources.getString(R.string.my_profile_title)
+        }
+        binding.toolbar.setOnClickListener {
+//            onBackPressed()
+            // Navigate back to the previous activity
+            finish()
+        }
+    }
+
+    fun setUserDataInUI(user: User) {
+        Glide
+            .with(this)
+            .load(user.image)
+            .centerCrop()
+            .placeholder(R.drawable.ic_user_place_holder)
+            .into(binding.civProfileImage)
+        binding.etName.setText(user.name)
+        binding.etEmail.setText(user.email)
+        if (user.mobile != 0L) {
+            binding.etMobileNumber.setText(user.mobile.toString())
+        }
+        // Set action bar title to the user's name
+        supportActionBar?.title = "${user.name}'s Profile"
     }
 }
